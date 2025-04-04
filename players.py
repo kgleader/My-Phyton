cb876e8060649e43923a043029ee4f25ef3100af
Meerim <kgleader@gmail.com>
2025-04-04 05:30:25 +0000

    players

---
+++ players.py
@@ -0,0 +1,20 @@
+players = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"]
+
+# Используйте срезы для создания списка для Group 1
+g1 = players[0:2]
+# Используйте срезы для создания списка для Group 2
+g2 = players[2:4]
+# Используйте срезы для создания списка для Group 3
+g3 = players[4:6]
+
+# отобразить 1-ю группу
+print("Group 1:")
+print(players[0:2])
+
+# отобразить 2-ю группу
+print("Group 2:")
+print(players[2:4])
+
+# отобразить 3-ю группу
+print("Group 3:")
+print(players[4:6])
