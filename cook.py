66248f5dc28b88339f2af979b1258adff7df4d56
Meerim <kgleader@gmail.com>
2025-04-04 04:17:15 +0000

    cook

---
+++ cook.py
@@ -0,0 +1,13 @@
+# Список с завтраками
+breakfasts = [
+  'Donuts', 'Waffles', 'Yogurt', 
+  'Burrito', 'Toast']
+
+# Индекс элемента, который нужно заменить
+item = int(input())
+
+# Заменяем элемент на "Pancakes"
+breakfasts[item] = "Pancakes"
+
+# Показываем обновлённый список
+print(breakfasts)
---
+++ games.py
@@ -0,0 +1,8 @@
+# установленные игры
+games = ['Soccer', 'Tic Tac Toe', 'Snake', 'Puzzle', 'Rally']
+
+# принимаем выбор игрока в виде числа
+choice = int(input())
+
+# вывод соответствующей игры
+print(games[choice])
